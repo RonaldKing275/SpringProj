db85e72461889f22f46491532a0a7bc8a3b790f6
RonaldKing275 <ronaldking0429@gmail.com>
2025-12-01 12:42:11 +0100

    poprawki do lab8

--- src/main/java/pl/rsz/springproj/config/WebConfig.java
+++ src/main/java/pl/rsz/springproj/config/WebConfig.java
@@ -1,6 +1,8 @@
 package pl.rsz.springproj.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.DateFormatter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,4 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/error403").setViewName("error403");
     }
+
+    @Override//Tutaj można też zarejestrować globalne formatery
+    public void addFormatters(FormatterRegistry registry) {
+        //globalna obsługa daty w formularzach
+        registry.addFormatter(new DateFormatter("yyyy-MM-dd"));
+        registry.addFormatter(new DateFormatter("HH:mm:ss"));
+    }
+
 }
